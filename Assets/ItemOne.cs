ffa002c66742bfbc5ac784a4c4dbb227723641f3
Yayoaka <stuluc.brusete@gmail.com>
2025-05-15 16:19:07 +0200

    init branch

---
+++ Assets/ItemOne.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ItemOne : MonoBehaviour
+{
+    string nameItemOne;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Menu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject ui;
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        if (ui != null)
+        {
+            ui.SetActive(!ui.activeSelf);
+
+            if (ui.activeSelf)
+            {
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                Time.timeScale = 1f;
+            }
+        }
+        else
+        {
+            Debug.LogError("UI GameObject is not assigned in the inspector.");
+        }
+    }
+    
+    public void ReTry()
+    {
+        SceneManager.LoadScene("test");
+        LevelManager.stateGame = 0;
+    }
+    
+    public void HandleExitGame()
+    {
+        Application.Quit();
+    }
+}
